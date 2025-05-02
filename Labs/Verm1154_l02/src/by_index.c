172d0b35f5a6378d4e2012d4a62b3636530d18f9
Himanya Verma <145639852+Himanya10@users.noreply.github.com>
2025-05-01 23:14:26 -0400

    Add files via upload

---
+++ Labs/Verm1154_l02/src/by_index.c
@@ -0,0 +1,39 @@
+/**
+ * -------------------------------------
+ * @file  by_index.c
+ * Lab 2 Index Functions Source Code File
+ * -------------------------------------
+ * @author Himanya Verma, 169051154, Verm1154@mylaurier.ca
+ *
+ * @version 2025-01-06
+ *
+ * -------------------------------------
+ */
+#include <stdio.h>
+#include "by_index.h"
+
+void fill_values_by_index(int values[], int size) {
+
+    for(int i = 0; i < size; i++) {
+        values[i] = i + 1;
+    }
+}
+
+void fill_squares_by_index(int values[], long int squares[], int size) {
+
+    for(int i = 0; i < size; i++){
+        squares[i] = (long int)values[i] * values[i];
+    }
+
+}
+
+void print_by_index(int values[], long int squares[], int size) {
+
+    printf("Value  Square\n");
+    printf("-----  ----------\n");
+
+    for(int i = 0; i < size; i++){
+        printf("%5d %10ld\n", values[i], squares[i]);
+    }
+
+}
---
+++ Labs/Verm1154_l02/src/by_ptr.c
@@ -0,0 +1,38 @@
+/**
+ * -------------------------------------
+ * @file  by_ptr.c
+ * Lab 2 Pointer Functions Source Code File
+ * -------------------------------------
+ * @author Himanya Verma, 169051154, Verm1154@mylaurier.ca
+ *
+ * @version 2025-01-06
+ *
+ * -------------------------------------
+ */
+#include "by_ptr.h"
+
+void fill_values_by_ptr(int *values, int size) {
+
+    for(int i = 0; i < size; i++) {
+        *(values + i) = i + 1;
+    }
+}
+
+void fill_squares_by_ptr(int *values, long int *squares, int size) {
+
+    for(int i = 0; i < size; i++){
+        *(squares + i) = (long int)(*(values + i)) * (*(values + i));
+    }
+
+}
+
+void print_by_ptr(int *values, long int *squares, int size) {
+
+    printf("Value  Square\n");
+    printf("-----  ----------\n");
+
+    for(int i = 0; i < size; i++){
+        printf("%5d %10ld\n", values[i], squares[i]);
+    }
+
+}
