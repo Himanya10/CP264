bc79a9377ebc73d3e664dbffbe5b83829f44d690
Himanya Verma <145639852+Himanya10@users.noreply.github.com>
2025-05-01 23:49:26 -0400

    Add files via upload

---
+++ Assignments/A1/quadratic.h
@@ -0,0 +1,16 @@
+/*
+--------------------------------------------------
+Project: a1q3
+File:    quadratic.h
+Author:  Himanya Verma
+Version: 2025-01-14
+--------------------------------------------------
+*/
+#ifndef QUADRATIC_H
+#define QUADRATIC_H
+
+int solution_type(float a, float b, float c);
+float real_root_big(float a, float b, float c);
+float real_root_small(float a, float b, float c);
+
+#endif 
