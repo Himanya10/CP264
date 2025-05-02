1ef8292c52666bcffddf8cf7fb9c2ade24978915
Himanya Verma <145639852+Himanya10@users.noreply.github.com>
2025-05-01 23:56:33 -0400

    Add files via upload

---
+++ Assignments/A6/stack.h
@@ -0,0 +1,47 @@
+/**
+ * -------------------------------------
+ * @file  stack.h
+ * file description
+ * -------------------------------------
+ * @author Himanya Verma, 169051154, Verm1154@mylaurier.ca
+ *
+ * @version 2025-02-28
+ *
+ * -------------------------------------
+ */
+
+#ifndef STACK_H
+#define STACK_H
+#include "common.h"
+
+/*
+ * Define a structure STACK containing the length and pointer of the top node.
+ * int length - maintains the length of stack.
+ * NODE *top - pointer to the top node of linked list stack
+ */
+typedef struct stack
+{
+    int length;
+    NODE *top;
+} STACK;
+
+/*
+ * Push a node into a linked list stack
+ * @param STACK *sp - pointer to the stack
+ * @param NODE *np - pointer to the node.
+ */
+void push(STACK *sp, NODE *np);
+
+/*
+ * Pop and return the pointer of the removed top node
+ * @param STACK *sp - pointer to the stack
+ * @return - the reference of the removed node and set it's next to NULL
+ */
+NODE *pop(STACK *sp);
+
+/*
+ * clean the linked list stack
+ */
+void clean_stack(STACK *sp);
+
+#endif
